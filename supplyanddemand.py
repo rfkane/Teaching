8826d9a4ca30d6a8be7154bf67c92e3a17d9ec0f
rfkane <72682569+rfkane@users.noreply.github.com>
2020-10-11 16:46:13 +0900

    Add files via upload

---
+++ supplyanddemand.py
@@ -0,0 +1,26 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+B1 = 40
+B2 = 0
+
+Q=np.linspace(0,50,1000)
+Demand = 100-2*Q
+Supply1 = B1+3*Q
+Supply2 = B2+3*Q
+
+if B2 > B1:
+    u = "Decrease supply"
+else:
+    u = "Increased Supply"
+
+
+plt.plot(Q,Demand,label = "Demand")
+plt.plot(Q,Supply1,label= "Supply")
+plt.plot(Q,Supply2,label= u)
+plt.legend()
+plt.xlabel('Quantity')
+plt.ylabel('Price')
+plt.show()
+
+
